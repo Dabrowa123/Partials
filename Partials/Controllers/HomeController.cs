87033fe95833082eaf5c0b21c01777423b3aeb9f
Dabrowa123 <dabrowa.szymon@gmail.com>
2017-04-22 19:47:57 +0100

    Partial Views
    Learning and repetition about partial views

---
+++ Partials/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Partials.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Partials.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Przyklad()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            List<Kurs> kursy = new List<Kurs>
+            {
+                new Kurs { nazwa= "1caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "2caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "3caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "4caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "5caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "6caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "7caskmcna", opis= "knasdlkcna" },
+                new Kurs { nazwa= "8caskmcna", opis= "knasdlkcna" },
+            };
+
+            return View(kursy);
+        }
+
+        [ChildActionOnly]
+        public ActionResult ShowTimePartial()
+        {
+            var time = DateTime.Now.ToLongTimeString();
+            return PartialView("_PartialTime", time);
+        }
+
+        public string ShowTime()
+        {
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
---
+++ Partials/Models/Kurs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Partials.Models
+{
+    public class Kurs
+    {
+        public string nazwa { get; set; }
+        public string opis { get; set; }
+    }
+}
